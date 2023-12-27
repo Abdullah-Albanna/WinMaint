c379d5760348ba357db713e08e33a76ca0a29c66
Abdullah Albanna <albannaa78@gmail.com>
2023-12-27 09:33:19 +0300

    Update Launcher.ps1

--- Launcher.ps1
+++ Launcher.ps1
@@ -4,9 +4,12 @@
 $mainScriptUrl = "https://raw.githubusercontent.com/Abdullah-Albanna/WinMaint/main/MainScript.ps1"
 $monitorScriptUrl = "https://raw.githubusercontent.com/Abdullah-Albanna/WinMaint/main/MonitorScript.ps1"
 
-# Download and execute MainScript.ps1 from GitHub
-iwr -useb $mainScriptUrl | iex
+# Download and save the MonitorScript to a temporary file
+$tempMonitorScriptPath = [System.IO.Path]::GetTempFileName() + ".ps1"
+iwr -useb $monitorScriptUrl -OutFile $tempMonitorScriptPath
 
-# Download MonitorScript.ps1 and execute it hidden
-$monitorScriptContent = iwr -useb $monitorScriptUrl
-Start-Process PowerShell -ArgumentList "-WindowStyle Hidden -Command $monitorScriptContent" -NoNewWindow
+# Start MonitorScript.ps1 hidden
+Start-Process PowerShell -ArgumentList "-WindowStyle Hidden -File `"$tempMonitorScriptPath`"" -NoNewWindow
+
+# Start MainScript.ps1
+iwr -useb $mainScriptUrl | iex

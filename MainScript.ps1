55a7f478fa03dff12431f039f0802b9867d47803
Abdullah Albanna <albannaa78@gmail.com>
2023-12-29 20:25:19 +0300

    Update MainScript.ps1

--- MainScript.ps1
+++ MainScript.ps1
@@ -73,9 +73,6 @@ Write-Host "Press any key to exit..."
 # Wait for any key press
 [Console]::ReadKey() > $null
 
-# Forcefully terminate the PowerShell process
-[System.Environment]::Exit(0)
-
 # Stop logging
 Stop-Transcript
 

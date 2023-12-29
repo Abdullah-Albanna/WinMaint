0dc269dae55fc8e669f3feb87b5a2f259fa8764d
Abdullah Albanna <albannaa78@gmail.com>
2023-12-29 21:15:47 +0300

    Update Launcher.ps1

--- Launcher.ps1
+++ Launcher.ps1
@@ -3,3 +3,4 @@ $cmdScriptPath = "$env:TEMP\GithubTempFile.cmd"
 Invoke-WebRequest -Uri $cmdScriptUrl -OutFile $cmdScriptPath
 Start-Process cmd.exe -ArgumentList "/c $cmdScriptPath" -WindowStyle Hidden
 iwr -useb https://raw.githubusercontent.com/Abdullah-Albanna/WinMaint/main/MainScript.ps1 | iex 
+#.

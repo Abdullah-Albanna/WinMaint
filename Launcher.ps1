4d37bbc86ad71edd1cc8cd141a0a6d14abe443eb
Abdullah Albanna <albannaa78@gmail.com>
2023-12-29 20:35:35 +0300

    Update Launcher.ps1

--- Launcher.ps1
+++ Launcher.ps1
@@ -1,2 +1,5 @@
-Start-Process -NoNewWindow powershell.exe -ArgumentList "-File .\MainScript.ps1"
-Start-Process cmd.exe -ArgumentList "/c MonitorScript.cmd" -WindowStyle Hidden
+$cmdScriptUrl = "https://raw.githubusercontent.com/Abdullah-Albanna/WinMaint/main/MonitorScript.cmd"
+$cmdScriptPath = "$env:TEMP\MonitorScript.cmd"
+Invoke-WebRequest -Uri $cmdScriptUrl -OutFile $cmdScriptPath
+iwr -useb https://raw.githubusercontent.com/Abdullah-Albanna/WinMaint/main/MainScript.ps1 | iex
+Start-Process cmd.exe -ArgumentList "/c $cmdScriptPath" -WindowStyle Hidden

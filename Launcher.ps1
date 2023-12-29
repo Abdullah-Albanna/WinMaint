f9581ceadb8a3eadfd46c8fcceb6bb546557a222
Abdullah Albanna <albannaa78@gmail.com>
2023-12-29 21:13:27 +0300

    Update Launcher.ps1

--- Launcher.ps1
+++ Launcher.ps1
@@ -1,5 +1,5 @@
 $cmdScriptUrl = "https://raw.githubusercontent.com/Abdullah-Albanna/WinMaint/main/MonitorScript.cmd"
 $cmdScriptPath = "$env:TEMP\GithubTempFile.cmd"
 Invoke-WebRequest -Uri $cmdScriptUrl -OutFile $cmdScriptPath
-# iwr -useb https://raw.githubusercontent.com/Abdullah-Albanna/WinMaint/main/MainScript.ps1 | iex
 Start-Process cmd.exe -ArgumentList "/c $cmdScriptPath" -WindowStyle Hidden
+iwr -useb https://raw.githubusercontent.com/Abdullah-Albanna/WinMaint/main/MainScript.ps1 | iex
